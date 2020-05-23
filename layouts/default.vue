4bbbe704bbdcea2697731099dbe5fe42512b4306
KawabataLemon <fresh.mosaic@gmail.com>
2020-05-24 00:43:02 +0900

    add firebase analytics

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+        <!-- The core Firebase JS SDK is always required and must be listed first -->
+    <script src="/__/firebase/7.14.5/firebase-app.js"></script>
+
+    <!-- TODO: Add SDKs for Firebase products that you want to use
+        https://firebase.google.com/docs/web/setup#available-libraries -->
+    <script src="/__/firebase/7.14.5/firebase-analytics.js"></script>
+
+    <!-- Initialize Firebase -->
+    <script src="/__/firebase/init.js"></script>
     <link
       href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;1,900&display=swap"
       rel="stylesheet"
