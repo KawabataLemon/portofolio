cd859cc2ef56d79e720a19120c0820b23d490a1a
KawabataLemon <fresh.mosaic@gmail.com>
2020-05-23 20:01:03 +0900

    add socials;

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,6 +2,28 @@
   .container
     navigation-header(:current-page="'contact'")
     section.contents
+      //- .leftContents
+      //-   .contentsTitle
+      //-     span mail
+      //-     .mailForm
+      //-       .fromAccount
+      //-       .mailAddress
+      //-       .mailContents
+      //-       .submitButton
+      .rightContents
+        .contentsTitle
+          span Socials
+        .contentsContainer
+          a(href="https://www.facebook.com/LemonKawabata").socialAccount
+            .socialIcon
+            .socialTitle Facebook
+          a(href="https://twitter.com/Kawabata_Lemon").socialAccount
+            .socialIcon
+            .socialTitle Twitter
+          a(href="https://www.instagram.com/kawabatalemon/").socialAccount
+            .socialIcon
+            .socialTitle Instagram
+
     navigation-footer(:current-page="'contact'")
 </template>
 
@@ -31,6 +53,35 @@ export default Vue.extend({
   min-height: 100vh;
   background: linear-gradient(to left, $color-primary, $color-secondary);
 
-   .contents{}
+   .contents{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-content: center;
+    margin-right:12px;
+    margin-left: 12px;
+      .contentsTitle {
+        font-size: 60px;
+        color: white;
+        @include lato;
+        font-weight: bold;
+        margin-bottom: 12px;
+      }
+
+      .contentsContainer {
+        margin-left: 32px;
+        a {
+          text-decoration: none;
+        }
+        .socialTitle {
+          font-size: 44px;
+          color: white;
+          transition: 400ms;
+          &:hover {
+            color: $color-secondary;
+          }
+        }
+      }
+   }
 }
 </style>
